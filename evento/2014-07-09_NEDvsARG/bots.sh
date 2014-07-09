e96c2584a6790d64b511378b64a00708164f80a5
Emerson Rocha Luiz <emerson@alligo.com.br>
2014-07-09 20:21:11 +0000

    evento.2014-07-09_NEDvsARG: Adicionado bot 03

--- evento/2014-07-09_NEDvsARG/bots.sh
+++ evento/2014-07-09_NEDvsARG/bots.sh
@@ -3,11 +3,16 @@
 twitter_to_csv --api-key NKPlqqMDLXr8nKHeF2KoJKYmM --api-secret UHwjNMrlVlFFOOij7D27acB6RLbrMpSvAeG0RUYSZQDdOTZVH9 --access-token 34828870-KQKmaf5A8xWxrmgWpbHaN1staxsXl2cHAnCUPpJzw --access-token-secret nDVwIoW4HBDKcEEofQhSyDYgXwq44MI8ZoSJVaocZHrYM  --json NEDvsARG_01.json --filter "#WorldCup2014,NEDvsARG,ARGvsNED,#NEDARG,#NED,#ARG,Argentina,Netherlands,holanda"
 
 # Bot 02 (key @alligoweb)
-twitter_to_csv --api-key 1Fe3P5gTm4VUo28VcBUIeQDo1 --api-secret iZp7S02ECqKnO1LgPNnzNt4t4kaqzsTxv4uACyAb6XhPQpf0fU --access-token 1051259412-lLCLPnimDeBxo8GS2IP1pJjSxkHN7KjMmWwx2Ab --access-token-secret 2GClMHAlgUymWClu9HGaQPiD5m5smtxkFNr8px2UY2kw4  --json NEDvsARG_02.json --filter "galvao,neymar,Romero,Zabaleta,Demichelis,Garay,Rojo,Biglia,Mascherano,Pérez,Higuaín,Messi,Lavezzi,Cillessen,D. Kuyt,R. Vlaar,S. De Vrij,Martins,D. Blind,Georginio,Sneijder,De Jong,Robben,Persie"
+twitter_to_csv --api-key 1Fe3P5gTm4VUo28VcBUIeQDo1 --api-secret iZp7S02ECqKnO1LgPNnzNt4t4kaqzsTxv4uACyAb6XhPQpf0fU --access-token 1051259412-lLCLPnimDeBxo8GS2IP1pJjSxkHN7KjMmWwx2Ab --access-token-secret 2GClMHAlgUymWClu9HGaQPiD5m5smtxkFNr8px2UY2kw4  --json NEDvsARG_02.json --filter "Romero,Zabaleta,Demichelis,Garay,Rojo,Biglia,Mascherano,Pérez,Higuaín,Lavezzi,Cillessen,D. Kuyt,R. Vlaar,S. De Vrij,Martins,D. Blind,Georginio,Sneijder,De Jong,Persie"
 
+# Bot 03 (key @movimentohermes)
+twitter_to_csv --api-key shv9DD8NGH2ZErqY11706MlVg --api-secret pfyidfCmUvugiPcDPEL0vNJk4Kvk7SOR3cQkBQTuiPHVdx6NMZ --access-token 213063386-ymDmNIDPP3IylFv1u1M03Q1jGpJ4Pfkgz2SQwqtV --access-token-secret Hh8QjVK8qGF1gVoZtpgitzijImU98eO9oQH7p5lsY1IsN  --json NEDvsARG_03.json --filter "Robben,Messi"
 
 
 Antigos
 --------------
 #Bot 01 (key @fititnt)
 twitter_to_csv --api-key NKPlqqMDLXr8nKHeF2KoJKYmM --api-secret UHwjNMrlVlFFOOij7D27acB6RLbrMpSvAeG0RUYSZQDdOTZVH9 --access-token 34828870-KQKmaf5A8xWxrmgWpbHaN1staxsXl2cHAnCUPpJzw --access-token-secret nDVwIoW4HBDKcEEofQhSyDYgXwq44MI8ZoSJVaocZHrYM  --json NEDvsARG_01.json --filter "#WorldCup2014,NEDvsARG,ARGvsNED,#NEDARG,#NED,#ARG,Argentina,Netherlands,holanda,Cillessen,D. Kuyt,R. Vlaar,S. De Vrij,B. Martins,D. Blind,Georginio,Sneijder,N. De Jong,A. Robben,Van Persie"
+
+# Bot 02 (key @alligoweb)
+twitter_to_csv --api-key 1Fe3P5gTm4VUo28VcBUIeQDo1 --api-secret iZp7S02ECqKnO1LgPNnzNt4t4kaqzsTxv4uACyAb6XhPQpf0fU --access-token 1051259412-lLCLPnimDeBxo8GS2IP1pJjSxkHN7KjMmWwx2Ab --access-token-secret 2GClMHAlgUymWClu9HGaQPiD5m5smtxkFNr8px2UY2kw4  --json NEDvsARG_02.json --filter "galvao,neymar,Romero,Zabaleta,Demichelis,Garay,Rojo,Biglia,Mascherano,Pérez,Higuaín,Messi,Lavezzi,Cillessen,D. Kuyt,R. Vlaar,S. De Vrij,Martins,D. Blind,Georginio,Sneijder,De Jong,Robben,Persie"
